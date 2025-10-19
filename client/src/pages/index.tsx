71e238ad96dea6505586262efe95c33b4682b262
chrislurohealth <chris@lurohealth.com>
2025-10-19 14:19:56 -0700

    make upper

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -388,7 +388,7 @@ export default function Home() {
         <input
           className="border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           onChange={(e) => {
-            peerId.current = e.target.value;
+            peerId.current = e.target.value.toUpperCase();
             console.log(peerId.current);
           }}
           placeholder="Peer ID"
